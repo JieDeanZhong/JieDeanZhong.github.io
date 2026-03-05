b0356171f879d28299603c49b7329ce9168cf118
DeanDean <amsterdamsterdam@outlook.com>
2026-03-05 12:04:54 +0800

    Update ThemeSwitch.tsx

--- components/ThemeSwitch.tsx
+++ components/ThemeSwitch.tsx
@@ -61,14 +61,14 @@ export default function ThemeSwitch() {
           leaveFrom="transform opacity-100 scale-100"
           leaveTo="transform opacity-0 scale-95"
         >
-          <MenuItems className="bg-white dark:bg-gray-800 ring-black/5 dark:ring-white/10 absolute right-0 z-10 mt-2 w-44 origin-top-right rounded-md p-1 shadow-lg ring-1 focus:outline-none">
+          <MenuItems className="absolute right-0 z-10 mt-2 w-44 origin-top-right rounded-md bg-white p-1 shadow-lg ring-1 ring-black/5 focus:outline-none dark:bg-gray-800 dark:ring-white/10">
             <RadioGroup value={resolvedTheme ?? theme} onChange={setTheme}>
               <MenuItem>
                 {({ active }) => (
                   <Radio
                     className={[
                       active ? 'bg-gray-100 dark:bg-gray-700' : '',
-                      'text-gray-900 dark:text-gray-100 flex cursor-pointer items-center gap-2 rounded-md px-2 py-2 text-sm',
+                      'flex cursor-pointer items-center gap-2 rounded-md px-2 py-2 text-sm text-gray-900 dark:text-gray-100',
                     ].join(' ')}
                     value="light"
                   >
@@ -83,7 +83,7 @@ export default function ThemeSwitch() {
                   <Radio
                     className={[
                       active ? 'bg-gray-100 dark:bg-gray-700' : '',
-                      'text-gray-900 dark:text-gray-100 flex cursor-pointer items-center gap-2 rounded-md px-2 py-2 text-sm',
+                      'flex cursor-pointer items-center gap-2 rounded-md px-2 py-2 text-sm text-gray-900 dark:text-gray-100',
                     ].join(' ')}
                     value="dark"
                   >
@@ -98,7 +98,7 @@ export default function ThemeSwitch() {
                   <Radio
                     className={[
                       active ? 'bg-gray-100 dark:bg-gray-700' : '',
-                      'text-gray-900 dark:text-gray-100 flex cursor-pointer items-center gap-2 rounded-md px-2 py-2 text-sm',
+                      'flex cursor-pointer items-center gap-2 rounded-md px-2 py-2 text-sm text-gray-900 dark:text-gray-100',
                     ].join(' ')}
                     value="system"
                   >
