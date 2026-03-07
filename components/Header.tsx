1940393482ce4c8575f30e28e555a4a05ea610e8
Dean <jiezhong@Deans-MacBook-Air.local>
2026-03-08 02:26:25 +0800

    signature renewing

--- components/Header.tsx
+++ components/Header.tsx
@@ -24,7 +24,7 @@ const Header = () => {
         <div className="flex items-center">
           <div className="flex items-center">
             <Image
-              src="/static/images/signature-icon.png"
+              src="/static/images/signature-latest.png"
               alt="Jie Dean Zhong signature"
               width={220}
               height={70}
